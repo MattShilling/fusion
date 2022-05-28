3f59ca01fe115c749c791cb4437a1e0717c9a0cf
Matt Shilling <mattthewshilling@gmail.com>
2022-05-28 04:48:17 -0700

    Important stuff defined first.

--- reactions.cc
+++ reactions.cc
@@ -4,49 +4,11 @@
 
 #include <vector>
 
-static Atom pluck_atom(Atom search, std::vector<Atom> &soup) {
-    Atom result(0, 0, 0);
-    for (ssize_t i = 0; i < soup.size(); i++) {
-        if (soup[i].protons() == search.protons() &&
-            soup[i].electrons() == search.electrons()) {
-            result = soup[i];
-            soup.erase(soup.begin() + i);
-            return result;
-        }
-    }
-
-    return result;
-}
+static Atom pluck_atom(Atom search, std::vector<Atom> &soup);
 
 static bool consume_atoms_if_all_exist(
     std::vector<Atom> atoms_to_consume,
-    std::vector<Atom> &soup) {
-    std::vector<Atom> plucked_atoms = {};
-    for (Atom atom_to_consume : atoms_to_consume) {
-        Atom result = pluck_atom(atom_to_consume, soup);
-        if (!result.Exists()) {
-            // We couldn't find the atom we're looking for, no point
-            // in looking further.
-            break;
-        } else {
-            plucked_atoms.push_back(result);
-        }
-    }
-
-    bool consumed_all_atoms =
-        plucked_atoms.size() == atoms_to_consume.size();
-
-    if (!consumed_all_atoms) {
-        // We were not able to pluck all the atoms from the soup
-        // that we were instructed to consume. Put the atoms we
-        // removed back into the soup.
-        for (Atom atom_to_put_back : plucked_atoms) {
-            soup.push_back(atom_to_put_back);
-        }
-    }
-
-    return consumed_all_atoms;
-}
+    std::vector<Atom> &soup);
 
 double Reactions::DeuteriumDeuterium::react(
     std::vector<Atom> &soup) {
@@ -58,8 +20,8 @@ double Reactions::DeuteriumDeuterium::react(
 
     std::random_device r;
     std::default_random_engine e1(r());
-    // The D–D reaction actually has two branches, each occurring
-    // with an approximately equal likelihood.
+    // The D–D reaction has two branches, each occurring with an
+    // approximately equal likelihood.
     std::uniform_int_distribution<size_t> uniform_dist(
         FirstReactionBranch,
         LastReactionBranch);
@@ -112,4 +74,48 @@ double Reactions::DeuteriumTritium::react(std::vector<Atom> &soup) {
     energy = 17.6 * MeV;
 
     return energy;
+}
+
+static Atom pluck_atom(Atom search, std::vector<Atom> &soup) {
+    Atom result(0, 0, 0);
+    for (ssize_t i = 0; i < soup.size(); i++) {
+        if (soup[i].protons() == search.protons() &&
+            soup[i].electrons() == search.electrons()) {
+            result = soup[i];
+            soup.erase(soup.begin() + i);
+            return result;
+        }
+    }
+
+    return result;
+}
+
+static bool consume_atoms_if_all_exist(
+    std::vector<Atom> atoms_to_consume,
+    std::vector<Atom> &soup) {
+    std::vector<Atom> plucked_atoms = {};
+    for (Atom atom_to_consume : atoms_to_consume) {
+        Atom result = pluck_atom(atom_to_consume, soup);
+        if (!result.Exists()) {
+            // We couldn't find the atom we're looking for, no point
+            // in looking further.
+            break;
+        } else {
+            plucked_atoms.push_back(result);
+        }
+    }
+
+    bool consumed_all_atoms =
+        plucked_atoms.size() == atoms_to_consume.size();
+
+    if (!consumed_all_atoms) {
+        // We were not able to pluck all the atoms from the soup
+        // that we were instructed to consume. Put the atoms we
+        // removed back into the soup.
+        for (Atom atom_to_put_back : plucked_atoms) {
+            soup.push_back(atom_to_put_back);
+        }
+    }
+
+    return consumed_all_atoms;
 }
